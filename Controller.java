46c295761a83bdbd5fef31c1e5d38eef8d013844
FrRushu <frushu@gmail.com>
2017-05-07 00:01:13 +0600

    FXML Introduction
    i) Adding multiple element 
    ii) Simple click event associated with button

---
+++ Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.scene.control.Button;
+
+//control the hole thing
+public class Controller {
+
+    public Button button;
+
+    public void buttonClicked(){
+        System.out.println("The button is clicked");
+    }
+
+
+}
